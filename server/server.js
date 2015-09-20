f0e88f4ee6f0f951819afc55f53e97a1fd4afc9c
Nate Willard <nate.a.willard@gmail.com>
2015-09-20 09:07:55 -0700

    Add basic get/post handling to api/weights on server

--- server/server.js
+++ server/server.js
@@ -1,19 +1,41 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var mongoose = require('mongoose');
 var path = require('path');
+var mongoose = require('mongoose');
+var Weight = require('./weightModel.js');
+var User = require('./userModel.js');
+var db = require('./db_conf.js');
+var session = require('express-session');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( {extended: true} ));
+app.use(session({
+  secret: 'oogabooga',
+  cookie: { maxAge: (60000 * 24 * 30) }
+}));
 app.use(express.static(path.join(__dirname,'../client')));
 
 var port = 4568;
+app.get('/api/weights', function(req, res) {
+  console.log(req.sessionID);
+  Weight.find({sessionID: req.sessionID}, function(err, result) {
+    if (err) { console.log(err); }
+    res.status(200).send(result);
+  });
+});
+
 app.post('/api/weights', function(req, res) {
-  console.log('req',req.body);
+  console.log(req.body);
+  var myWeight = new Weight({weight: req.body.weight, sessionID: req.sessionID}/*{weight: req.body, sessionID: req.sessionID}*/);
+  /*User.find({sessionID: req.sessionID}, function(err, result) {
+    if (err) { console.log(err); }
+    console.log(result);
+  });*/
+  myWeight.save(function(err) { if (err) { console.log('err', err); } });
   res.status(200).send("Functionality to be added later");
 });
 
-console.log('Shortly is listening on port');
+console.log('Shortly is listening on port '+port);
 app.listen(port);
 

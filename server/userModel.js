80dcf0ce9b93a2c3b7df7517ecc689a43187af4f
Nate Willard <nate.a.willard@gmail.com>
2015-09-19 21:28:23 -0700

    Add user schema and model for authentication

---
+++ server/userModel.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+var userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+userSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified(password)) {
+    return next();
+  }
+  bcrypt.hash(user.password, null, null, function(err, hash) {
+    if (err) { return next(err); }
+    user.password = hash;
+    next();
+  });
+});
+
+module.exports = mongoose.model('User', userSchema);

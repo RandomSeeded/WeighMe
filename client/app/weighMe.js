12172d6ae2ea4a7a7b114fb7920a024fc8223ba0
Nate Willard <nate.a.willard@gmail.com>
2015-09-20 09:57:04 -0700

    Add routes for login/signup

--- client/app/weighMe.js
+++ client/app/weighMe.js
@@ -5,4 +5,14 @@ angular.module('weighMe', [ 'weighMe.weights', 'weighMe.services', 'ngRoute' ])
       templateUrl: 'app/weights/weights.html',
       controller: 'weightController'
     })
-})
+    .when('/signup', {
+      templateUrl: 'app/auth/signup.html'
+    })
+    .when('/signin', {
+      templateUrl: 'app/auth/login.html'
+    })
+    .otherwise({
+      templateUrl: 'app/weights/weights.html',
+      controller: 'weightController'
+    })
+});

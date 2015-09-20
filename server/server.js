2ad0c6c738afbd628c0a4967c7eebf32740964c4
Nate Willard <nate.a.willard@gmail.com>
2015-09-19 18:59:40 -0700

    Restructure of files to add server folder, temporary addition of response message

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var mongoose = require('mongoose');
+var path = require('path');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( {extended: true} ));
-app.use(express.static(__dirname+'/client'));
+app.use(express.static(path.join(__dirname,'../client')));
 
 app.post('/api/weights', function(req, res) {
   console.log('req',req.body);
-  res.send(200);
+  res.send(200, "Functionality to be added later");
 });
 
 console.log('Shortly is listening on 4568');
